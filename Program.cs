7f1e8e63193a6785731dc9876ea77511d186af25
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-09-08 16:16:01 -0300

    Criando a classe posicao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using XadrezConsole.Tabuleiro;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Posicao P = new(3, 4);
+
+        }
+    }
+}
---
+++ Tabuleiro/Posicao.cs
@@ -0,0 +1,16 @@
+﻿
+namespace XadrezConsole.Tabuleiro {
+    class Posicao {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString() {
+            return Linha + ", " + Coluna;
+        }
+    }
+}
